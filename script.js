74c58c450e487a3795f96f6a1a1dc3d061da0434
GabrielGame <gabidubinski05@gmail.com>
2026-06-17 19:42:07 -0300

    Adicionando no código ValidarRetirada e do estoque atual e adicionando quantidadeRetirada

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ const tbody = document.querySelector("#lista-materiais tbody");
 
 let idEdicao = null;
 
+function validarRetirada(estoqueAtual, quantidadeRetirada) {
+    if (quantidadeRetirada <= 0) {
+        return false;
+    }
+
+    if (quantidadeRetirada > estoqueAtual) {
+        return false;
+    }
+
+    return true;
+}
+
 async function carregarMateriais() {
     const resposta = await fetch(API_URL);
     const materiais = await resposta.json();
@@ -20,8 +32,36 @@ async function carregarMateriais() {
             <td>${material.nome}</td>
             <td>${material.quantidade}</td>
             <td>
-                <button onclick="editarMaterial('${material.id}')">✏️ Editar</button>
-                <button onclick="excluirMaterial('${material.id}')">🗑️ Excluir</button>
+                <input
+                    type="number"
+                    id="input-retirada"
+                    min="1"
+                    placeholder="Qtd"
+                >
+            </td>
+            <td>
+                <button
+                    type="button"
+                    onclick="baixarMaterial('${material.id}', ${material.quantidade}, this)"
+                    class="btn-baixar"
+                >
+                    Baixar
+                </button>
+
+                <button
+                    type="button"
+                    onclick="editarMaterial('${material.id}')"
+                >
+                    Editar
+                </button>
+
+                <button
+                    type="button"
+                    onclick="excluirMaterial('${material.id}')"
+                    class="btn-excluir"
+                >
+                    Excluir
+                </button>
             </td>
         `;
 
@@ -77,6 +117,33 @@ async function editarMaterial(id) {
 async function excluirMaterial(id) {
     await fetch(`${API_URL}/${id}`, {
         method: "DELETE"
+    });
+
+    carregarMateriais();
+}
+
+async function baixarMaterial(id, estoqueAtual, botao) {
+    const linha = botao.closest("tr");
+
+    const inputRetirada = linha.querySelector("#input-retirada");
+
+    const quantidadeRetirada = Number(inputRetirada.value);
+
+    if (!validarRetirada(estoqueAtual, quantidadeRetirada)) {
+        alert("Quantidade inválida.");
+        return;
+    }
+
+    const novaQuantidade = estoqueAtual - quantidadeRetirada;
+
+    await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            quantidade: novaQuantidade
+        })
     });
 
     carregarMateriais();
