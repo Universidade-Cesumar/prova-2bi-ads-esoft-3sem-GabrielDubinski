effaba0c2454f6ef1fe3d406df86722114d35d48
GabrielGame <gabidubinski05@gmail.com>
2026-07-02 14:31:01 -0300

    bloqueio de valores negativos no input de retirada

--- script.js
+++ script.js
@@ -86,6 +86,10 @@ async function carregarMateriais() {
                 <td>${material.nome}</td>
                 <td>${material.quantidade}</td>
                 <td>
+
+                inputRetirada.addEventListener("input", (e) => {
+    if (e.target.value < 0) e.target.value = 0;
+});
                     <input type="number" id="input-retirada" min="1" placeholder="Qtd">
                 </td>
                 <td>
